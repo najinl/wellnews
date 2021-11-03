9abcf3cf95568d2da140791ee2193d983d9ab2b4
Anthony Iacono <72999840+anthony-iacono@users.noreply.github.com>
2021-11-03 15:13:01 -0700

    Clean up App

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,31 +1,31 @@
-import React, { useState, useEffect } from 'react';
-import Feed from '../Feed/Feed';
-import Form from '../Form/Form';
+import React, { useState } from 'react';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import Form from '../Form/Form';
+import Feed from '../Feed/Feed';
 import './App.css';
-// import { CleanArticle, fetchNewsData, getSentiment } from '../../apiCalls'
 
 const App = (): JSX.Element => {
+  const [ userSentiment, setUserSentiment ] = useState<number | null>(null);
 
-  const [ userSentiment, setUserSentiment ] = useState<number | null>(null)
-
-  const changeUserSentiment = (newSentiment: number) => {
-    setUserSentiment(newSentiment)
+  const updateUserSentiment = (newSentiment: number) => {
+    setUserSentiment(newSentiment);
   }
 
   return (
     <div className="App">
       <div className="app-container">
         <header className="App-header">
-          <h1 className="header-text">Well<span className="header-text-2">News</span></h1>
+          <h1 className="header-text">
+            Well<span className="header-text-2">News</span>
+          </h1>
         </header>
         <Router>
           <Switch>
             <Route exact path="/">
-              <Form changeUserSentiment={changeUserSentiment}/>
+              <Form updateUserSentiment={ updateUserSentiment } />
             </Route>
             <Route path="/feed/">
-              <Feed userSentiment={ userSentiment }/>
+              <Feed userSentiment={ userSentiment } />
             </Route>
           </Switch>
         </Router>
