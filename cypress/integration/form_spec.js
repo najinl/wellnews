5c51b3611b33f42fb560182577790647b98c6770
Anthony Iacono <72999840+anthony-iacono@users.noreply.github.com>
2021-11-04 10:14:32 -0700

    Remove token variables and warnings from tests

--- cypress/integration/form_spec.js
+++ cypress/integration/form_spec.js
@@ -1,11 +1,8 @@
 describe ('Form user flow', () => {
   beforeEach(() => {
-    // add your Dandelion API token here; remove before merging to main
-    const dandelionToken = '488823ba0f914b1f8eddc319191cbc7a';
-    if (dandelionToken) { console.log('Token in use; remember to remove before opening a PR')}
     cy.intercept("https://api.nytimes.com/svc/topstories/v2/home.json?api-key=GKUzDD1VY9ssjZ1AGusX3ci6AeoXCaSr", {fixture: 'nyt-article-fetch.json'})
-      .intercept(`https://api.dandelion.eu/datatxt/sent/v1/?lang=en&text=A new biography by Debby Applegate recounts the story of Polly Adler, who arrived in America from Russia at 13 and became New Yorks most successful brothel owner, befriending mobsters, policemen, politicians and writers&token=${dandelionToken}`, {fixture: 'manhattan-madam-sentiment.json'})
-      .intercept(`https://api.dandelion.eu/datatxt/sent/v1/?lang=en&text=The magazines Ethicist columnist on responding to infidelity&token=${dandelionToken}`, {fixture: 'affair-sentiment.json'})
+      .intercept('https://api.dandelion.eu/datatxt/sent/v1/?lang=en&text=A new biography by Debby Applegate recounts the story of Polly Adler, who arrived in America from Russia at 13 and became New Yorks most successful brothel owner, befriending mobsters, policemen, politicians and writers&token=488823ba0f914b1f8eddc319191cbc7a', {fixture: 'manhattan-madam-sentiment.json'})
+      .intercept('https://api.dandelion.eu/datatxt/sent/v1/?lang=en&text=The magazines Ethicist columnist on responding to infidelity&token=488823ba0f914b1f8eddc319191cbc7a', {fixture: 'affair-sentiment.json'})
      .visit('/')
   })
 
