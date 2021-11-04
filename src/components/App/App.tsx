68d64687bf5c02892fea5abe033961bc87c28b46
Natalia Zamboni Vergara <nzamboniv@gmail.com>
2021-11-04 11:59:26 -0600

    Transfer articles to App & keep sortingn in Feed to use Router for single articles

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,11 +1,42 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import { getArticles, getSentiment } from '../../apiCalls';
+import { CleanedArticle } from '../../Models';
 import Form from '../Form/Form';
 import Feed from '../Feed/Feed';
+import Article from '../Article/Article';
 import './App.css';
 
 const App = (): JSX.Element => {
+  const [articles, setArticles] = useState<CleanedArticle[]>([]);
+  const [error, setError] = useState('');
   const [ userSentiment, setUserSentiment ] = useState<number | null>(null);
+
+  useEffect((): void => {
+    getArticles()
+      .then((cleanedArticles: CleanedArticle[]): void => {
+        getSentimentScores(cleanedArticles)
+          .then((response: number[]) => {
+
+            const scoredArticles = cleanedArticles.map((article, i) => {
+               article.sentiment = response[i];
+               return article;
+            });
+
+            setArticles(scoredArticles);
+          });
+      })
+      .catch(error => setError(error.message));
+  }, []);
+
+
+  const getSentimentScores = (cleanedArticles: CleanedArticle[]): Promise<number[]> => {
+    return Promise.all(
+      cleanedArticles.map((article: CleanedArticle) => {
+        return getSentiment(article.abstract)
+      })
+    );
+  };
 
   const updateUserSentiment = (userSentiment: number) => {
     setUserSentiment(userSentiment);
@@ -24,11 +55,42 @@ const App = (): JSX.Element => {
             <Route exact path="/">
               <Form updateUserSentiment={ updateUserSentiment } />
             </Route>
-            <Route path="/feed/">
-              <Feed userSentiment={ userSentiment } />
-            </Route>
+            <Route
+              exact path="/feed/"
+              render={() => {
+                return (
+                  <>
+                    <Feed
+                      userSentiment={ userSentiment }
+                      articles={ articles }/>
+                    { !articles.length && <h2>Loading.. </h2>}
+                    { error && <h2>{error}</h2> }
+                  </>
+                )
+              }}
+            />
+            <Route
+              path="/feed/:id"
+              render={({ match }) => {
+                const id = Number(match.params.id)
+                const singleArticle = articles.find(article => article.id === id)
+
+                if (singleArticle) {
+                  return (
+                    <Article
+                      title={ singleArticle.title }
+                      image={ singleArticle.multimedia.url }
+                      caption={ singleArticle.multimedia.caption }
+                      abstract={ singleArticle.abstract }
+                      key={ singleArticle.title }
+                    />
+                  )
+                }
+              }}
+            />
           </Switch>
         </Router>
+
       </div>
     </div>
   )
--- src/components/Feed/Feed.tsx
+++ src/components/Feed/Feed.tsx
@@ -1,99 +1,45 @@
-import { useState, useEffect } from 'react';
-import { getArticles, getSentiment } from '../../apiCalls';
 import { CleanedArticle } from '../../Models'
 import Card from '../Card/Card';
-import Article from '../Article/Article';
 import './Feed.css';
 
 interface FeedProps {
   userSentiment: number | null;
+  articles: CleanedArticle[];
 }
 
-const Feed = ({ userSentiment }: FeedProps): JSX.Element => {
-  const [articles, setArticles] = useState<CleanedArticle[]>([]);
-  const [error, setError] = useState('');
-  const [singleArticle, setSingleArticle] = useState<CleanedArticle | null>(null)
+const Feed = ({ userSentiment, articles }: FeedProps): JSX.Element => {
 
-  const getSentimentScores = (cleanedArticles: CleanedArticle[]): Promise<number[]> => {
-    return Promise.all(
-      cleanedArticles.map((article: CleanedArticle) => {
-        return getSentiment(article.abstract)
-      })
+  let sortedArticles : CleanedArticle[];
+
+  if (userSentiment === -1) {
+    sortedArticles = articles.sort((articleA, articleB) => {
+      return articleB.sentiment - articleA.sentiment;
+    })
+  } else if (userSentiment === 1) {
+    sortedArticles = articles.sort((articleA, articleB) => {
+      return articleA.sentiment - articleB.sentiment;
+    })
+  } else {
+    sortedArticles = articles.sort((articleA, articleB) => 0.5 - Math.random());
+  }
+
+
+   const articleCards = sortedArticles.map(article => {
+    return  <Card
+        title={ article.title }
+        image={ article.multimedia.url }
+        id={ article.id }
+        key={ article.title }
+      />
+    })
+
+    return (
+      <div className="articles-container">
+        <section className="articles-display">
+          {articleCards}
+        </section>
+      </div>
     );
-  };
-
-  useEffect((): void => {
-    getArticles()
-      .then((cleanedArticles: CleanedArticle[]): void => {
-        getSentimentScores(cleanedArticles)
-          .then((response: number[]) => {
-
-            const scoredArticles = cleanedArticles.map((article, i) => {
-               article.sentiment = response[i] || 0;
-               return article;
-            });
-            sortByUserSentiment(userSentiment, scoredArticles);
-          });
-      })
-      .catch(error => setError(error.message));
-  }, []);
-
-  const sortByUserSentiment = (userSentiment: number|null, scoredArticles: CleanedArticle []): void => {
-    let sortedArticles : CleanedArticle[] = [];
-
-    if (userSentiment === -1) {
-      sortedArticles = scoredArticles.sort((articleA, articleB) => {
-        return articleB.sentiment - articleA.sentiment;
-      })
-    } else if (userSentiment === 1) {
-      sortedArticles = scoredArticles.sort((articleA, articleB) => {
-        return articleA.sentiment - articleB.sentiment;
-      })
-    } else {
-      sortedArticles = scoredArticles.sort((articleA, articleB) => 0.5 - Math.random());
-    }
-
-    setArticles(sortedArticles)
-  }
-
-  const selectSingleArticle = (id: number) => {
-    const article = articles.find(article => article.id === id)
-    if (article) {
-      setSingleArticle(article)
-    }
-  }
-
-  const showFeed = ():void => {
-    setSingleArticle(null)
-  }
-
-  return (
-    <div className="articles-container">
-      <section className="articles-display">
-        { error && <h2>{ error }</h2> }
-        { !articles.length && <h1>Loading...</h1> }
-        { !singleArticle && articles.map(article =>
-          <Card
-            title={ article.title }
-            image={ article.multimedia.url }
-            id={ article.id }
-            selectSingleArticle={ selectSingleArticle }
-            key={ article.title }
-          />)
-        }
-        { singleArticle &&
-          <Article
-            title={ singleArticle.title }
-            image={ singleArticle.multimedia.url }
-            caption={ singleArticle.multimedia.caption }
-            abstract={ singleArticle.abstract }
-            showFeed={ showFeed }
-            key={ singleArticle.title }
-          />
-        }
-      </section>
-    </div>
-  );
 };
 
 export default Feed;
