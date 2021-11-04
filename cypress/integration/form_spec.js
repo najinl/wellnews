db1eed3c5c20d2e0110549bfacc5c49cc3acc5dd
Anthony Iacono <72999840+anthony-iacono@users.noreply.github.com>
2021-11-03 21:44:06 -0700

    Convert to testing selectors

--- cypress/integration/form_spec.js
+++ cypress/integration/form_spec.js
@@ -5,36 +5,36 @@ describe ('Form user flow', () => {
 
   it('should render the appropriate elements', () => {
     cy.contains('WellNews')
-      .get('h2').contains('how are you feeling today?')
-      .get('.happy-btn').should('be.visible')
-      .get('.neutral-btn').should('be.visible')
-      .get('.sad-btn').should('be.visible')
-      .get('.view-synopsis-button').should('be.visible')
+      .get('.cy-sentiment-question').contains('how are you feeling today?')
+      .get('.cy-happy-btn').should('be.visible')
+      .get('.cy-neutral-btn').should('be.visible')
+      .get('.cy-sad-btn').should('be.visible')
+      .get('.cy-view-synopsis-button').should('be.visible')
   })
 
   it('should take you to the news feed when the happy sentiment button is chosen', () => {
-    cy.get('.happy-btn').click()
+    cy.get('.cy-happy-btn').click()
       .url().should('include', '/feed')
   })
 
   it('should take you to the news feed when the neutral sentiment button is chosen', () => {
-    cy.get('.neutral-btn').click()
+    cy.get('.cy-neutral-btn').click()
       .url().should('include', '/feed')
   })
 
   it('should take you to the news feed when the sad sentiment button is chosen', () => {
-    cy.get('.sad-btn').click()
+    cy.get('.cy-sad-btn').click()
       .url().should('include', '/feed')
   })
 
   it('should show a synopsis of why you should choose your sentiment when the "Why we ask?" button is clicked', () => {
-    cy.get('.view-synopsis-button').click()
-      .get('.wellnews-synopsis').should('be.visible')
+    cy.get('.cy-view-synopsis-button').click()
+      .get('.cy-wellnews-synopsis').should('be.visible')
   })
 
   it('should hide the synopsis when the "Why we ask?" button is clicked again', () => {
-    cy.get('.view-synopsis-button').click()
-      .get('.view-synopsis-button').click()
-      .get('.wellnews-synopsis').should('not.be.visible')
+    cy.get('.cy-view-synopsis-button').click()
+      .get('.cy-view-synopsis-button').click()
+      .get('.cy-wellnews-synopsis').should('not.be.visible')
   })
 })
--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -22,7 +22,7 @@ const Form = ({ updateUserSentiment }: FormProps): JSX.Element => {
 
   return (
     <section className="questionnaire">
-      <h2>how are you feeling today?</h2>
+      <h2 className='cy-sentiment-question'>how are you feeling today?</h2>
       <div className="sentiment-selection">
         <div className="sentiments-container">
           <button className="happy-btn cy-happy-btn" name='1' aria-label="happy" title="happy" onClick={() => submitUserSentiment(1)}></button>
@@ -30,8 +30,8 @@ const Form = ({ updateUserSentiment }: FormProps): JSX.Element => {
           <button className="sad-btn cy-sad-btn" name='-1' aria-label="sad" title="sad" onClick={() => submitUserSentiment(-1)}></button>
         </div>
       </div>
-      <button className="view-synopsis-button" name="viewSynopsisButton" onClick={ showSynopsis }>Why we ask? ▽</button>
-      <article className="wellnews-synopsis" id="synopsisInfo">
+      <button className="view-synopsis-button cy-view-synopsis-button" name="viewSynopsisButton" onClick={ showSynopsis }>Why we ask? ▽</button>
+      <article className="wellnews-synopsis cy-wellnews-synopsis" id="synopsisInfo">
         <p>WellNews uses your mood to curate a list of articles with your mental health in mind.</p>
         <p>A negative mood will prioritize positive articles. <br/>A positive mood will result in a mix of positive, neutral, and negative news.</p>
         <p>If you are unsure, select a neutral mood for a mix of articles that lean towards the positive.</p>
