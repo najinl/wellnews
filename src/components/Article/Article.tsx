30a2484e714270da7c42c9a3878272cf028632fb
Joshua Horner <lesliewbusiness@gmail.com>
2021-11-05 08:36:06 -0600

    Add Article view to sentiment score branch

---
+++ src/components/Article/Article.tsx
@@ -0,0 +1,48 @@
+import { Link } from 'react-router-dom'
+import './Article.css';
+
+interface ArticleProps {
+  title: string
+  image: string
+  abstract: string
+  caption: string
+}
+
+const Article = ({ title, image, abstract, caption }: ArticleProps): JSX.Element => {
+
+  return (
+    <section className="single-article-container">
+      <div className="back-button-container">
+        <Link to="/feed">
+          ⇦ BACK
+        </Link>
+      </div>
+      <figure>
+        <img src={image} alt={caption} className="single-article-image"/>
+        <figcaption className="single-article-caption">{caption}</figcaption>
+      </figure>
+      <h2 className="single-article-title">{title}</h2>
+      <p>{abstract}</p>
+      <p>{placeholderText}</p>
+      <p>{placeholderText}</p>
+      <p>{placeholderText}</p>
+      <p>{placeholderText}</p>
+      <p>{placeholderText}</p>
+      <p>{placeholderText}</p>
+      <p>{placeholderText}</p>
+      <p>{placeholderText}</p>
+      <p>{placeholderText}</p>
+      <p>{placeholderText}</p>
+      <div className="back-button-container">
+        <Link to="/feed">
+          ⇦ BACK
+        </Link>
+      </div>
+    </section>
+  )
+}
+
+
+const placeholderText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Aliquam ut porttitor leo a diam sollicitudin tempor id eu. In arcu cursus euismod quis viverra nibh cras pulvinar mattis. A diam maecenas sed enim. Ipsum dolor sit amet consectetur. Mauris commodo quis imperdiet massa tincidunt nunc pulvinar sapien. Tellus integer feugiat scelerisque varius morbi. Ac placerat vestibulum lectus mauris ultrices eros in cursus turpis. Gravida neque convallis a cras semper auctor. Suspendisse in est ante in nibh mauris cursus mattis molestie."
+
+export default Article
