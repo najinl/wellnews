02174c590d76125798a95ad8c40eeb55d198a0e1
Anthony Iacono <72999840+anthony-iacono@users.noreply.github.com>
2021-11-08 17:15:59 -0800

    Add Header to Article

--- src/components/Article/Article.tsx
+++ src/components/Article/Article.tsx
@@ -1,6 +1,7 @@
 import { Link } from 'react-router-dom';
 import '../Header/Header.css';
 import './Article.css';
+import Header from '../Header/Header'
 
 const placeholderText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Aliquam ut porttitor leo a diam sollicitudin tempor id eu. In arcu cursus euismod quis viverra nibh cras pulvinar mattis. A diam maecenas sed enim. Ipsum dolor sit amet consectetur. Mauris commodo quis imperdiet massa tincidunt nunc pulvinar sapien. Tellus integer feugiat scelerisque varius morbi. Ac placerat vestibulum lectus mauris ultrices eros in cursus turpis. Gravida neque convallis a cras semper auctor. Suspendisse in est ante in nibh mauris cursus mattis molestie."
 
@@ -15,22 +16,22 @@ interface ArticleProps {
 const Article = ({ title, image, abstract, caption, selectedTopic }: ArticleProps): JSX.Element => {
 
   return (
-    <section className="single-article-container">
-      <h1 className="header-txt cy-header-txt">
-        Well<span className="header-text-2">News</span>
-      </h1>
-      <figure>
-        <img src={image} alt={caption} className="single-article-image cy-single-article-image"/>
-        <figcaption className="single-article-caption cy-single-article-caption">{caption}</figcaption>
-      </figure>
-      <h2 className="single-article-title cy-single-article-title">{title}</h2>
-      <p className="cy-single-article-abstract">{abstract}</p>
-      <p>{placeholderText}</p>
-      <p>{placeholderText}</p>
-      <p>{placeholderText}</p>
-      <p>{placeholderText}</p>
-      <p>{placeholderText}</p>
-    </section>
+    <>
+      <Header />
+      <section className="single-article-container">
+        <figure>
+          <img src={image} alt={caption} className="single-article-image cy-single-article-image"/>
+          <figcaption className="single-article-caption cy-single-article-caption">{caption}</figcaption>
+        </figure>
+        <h2 className="single-article-title cy-single-article-title">{title}</h2>
+        <p className="cy-single-article-abstract">{abstract}</p>
+        <p>{placeholderText}</p>
+        <p>{placeholderText}</p>
+        <p>{placeholderText}</p>
+        <p>{placeholderText}</p>
+        <p>{placeholderText}</p>
+      </section>
+    </>
   )
 }
 
