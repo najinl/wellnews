68eafd305f34a40f4e56c6fb2d715937f1370498
Natalia Zamboni Vergara <nzamboniv@gmail.com>
2021-11-27 19:03:23 -0700

    Remove extra div

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -5,19 +5,17 @@ import './Header.css';
 const Header = (): JSX.Element => {
   return (
     <header className="header">
-      <div className="header-elements-container">
-        <Link to='/'>
-          <button className="check-in-btn">
-            <span className="check-in-text">Check In</span>
-          </button>
-        </Link>
-        <h1 className="header-txt cy-header-txt">
-          Well<span className="header-text-2">News</span>
-        </h1>
-        <Link to='/search-topic'>
-          <button className="list-btn" aria-label="Browse by Topic"></button>
-        </Link>
-      </div>
+      <Link to='/'>
+        <button className="check-in-btn">
+          <span className="check-in-text">Check In</span>
+        </button>
+      </Link>
+      <h1 className="header-txt cy-header-txt">
+        Well<span className="header-text-2">News</span>
+      </h1>
+      <Link to='/search-topic'>
+        <button className="list-btn" aria-label="Browse by Topic"></button>
+      </Link>
     </header>
   )
 }
